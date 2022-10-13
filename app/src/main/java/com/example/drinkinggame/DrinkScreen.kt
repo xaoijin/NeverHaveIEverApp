4bee95adb59a65526cc45ce1e968751499c68626
Windx15 <17182149+windx15@users.noreply.github.com>
2022-10-13 15:16:29 -0400

    Made All the empty Activities

---
+++ app/src/main/java/com/example/drinkinggame/DrinkScreen.kt
@@ -0,0 +1,11 @@
+package com.example.drinkinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrinkScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drink_screen)
+    }
+}
---
+++ app/src/main/java/com/example/drinkinggame/QuestionSets.kt
@@ -0,0 +1,11 @@
+package com.example.drinkinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionSets : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_sets)
+    }
+}
