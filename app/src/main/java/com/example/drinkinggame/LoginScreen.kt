cad801d9e38bcf1d1c0f9f0978b9d2c263198f75
Windx15 <17182149+windx15@users.noreply.github.com>
2022-10-16 20:23:47 -0400

    Able to login account and added a animation background to login screen activity

--- app/src/main/java/com/example/drinkinggame/LoginScreen.kt
+++ app/src/main/java/com/example/drinkinggame/LoginScreen.kt
@@ -1,10 +1,12 @@
 package com.example.drinkinggame
+
+
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
@@ -20,6 +22,17 @@ class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+        val linearLayout = findViewById<LinearLayout>(R.id.loginlayout)
+        val animationDrawable = linearLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(500)
+        animationDrawable.setExitFadeDuration(4500)
+        animationDrawable.start()
+
+
+
         errorInfo = findViewById(R.id.ErrorInfo)
         lEmail = findViewById(R.id.emailInput)
         lPassword = findViewById(R.id.passwordInput)
@@ -29,13 +42,36 @@ class LoginScreen : AppCompatActivity() {
         errorInfo.visibility = View.INVISIBLE
 
 
-        bLogin.setOnClickListener { }
+        bLogin.setOnClickListener {
+            loginAccount()
+        }
         bRegister.setOnClickListener{
             registerAccount()
         }
     }
+    private fun loginAccount(){
+        val email = lEmail.text.toString()
+        val psw = lPassword.text.toString()
+        if (email.isEmpty() || psw.isEmpty()){
+            Toast.makeText(this, "Please Enter Email or Password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        Log.d("Main", "Email:$email")
+        Log.d("Main", "Password: $psw" )
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,psw)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d("Main", "Successfully Login: ${it.result?.user?.uid}")
+                val intent = Intent(this, InitialScreen::class.java)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                errorInfo.visibility = View.VISIBLE
+                Log.d("Main", "Failed to Login User: ${it.message}")
+            }
+    }
     private fun registerAccount(){
-         val email = lEmail.text.toString()
+        val email = lEmail.text.toString()
         val psw = lPassword.text.toString()
         if (email.isEmpty() || psw.isEmpty()){
             Toast.makeText(this, "Please Enter Text Email or Password", Toast.LENGTH_SHORT).show()
@@ -47,9 +83,11 @@ class LoginScreen : AppCompatActivity() {
             .addOnCompleteListener {
                 if (!it.isSuccessful) return@addOnCompleteListener
                 Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+                Toast.makeText(this, "Account Successfully Created", Toast.LENGTH_SHORT).show()
             }
             .addOnFailureListener {
                 Log.d("Main", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Email Address is already taken", Toast.LENGTH_SHORT).show()
             }
     }
 
--- app/src/main/java/com/example/drinkinggame/SplashScreen.kt
+++ app/src/main/java/com/example/drinkinggame/SplashScreen.kt
@@ -3,19 +3,30 @@ package com.example.drinkinggame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        auth = FirebaseAuth.getInstance()
+
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
         val someDelay = Thread{
             Thread.sleep(3000)
-            val intent  = Intent(this, LoginScreen::class.java)
-            startActivity(intent)
-            finish()
+            if(auth.currentUser == null){
+                val intent = Intent(this, LoginScreen::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                val intent = Intent(this, InitialScreen::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
         someDelay.start()
     }
