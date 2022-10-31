74a8ce3f84cce9ba28fa7a35a4dd5ee153175831
Windx15 <17182149+windx15@users.noreply.github.com>
2022-10-30 22:31:08 -0400

    Select functionality added and works in QuestionSets.kt

--- app/src/main/java/com/example/drinkinggame/EditQuestionSet.kt
+++ app/src/main/java/com/example/drinkinggame/EditQuestionSet.kt
@@ -2,8 +2,13 @@ package com.example.drinkinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class EditQuestionSet : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private var db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_question_set)
