136bf62e617616159fb145e4f140b3408a8aaec6
Windx15 <17182149+windx15@users.noreply.github.com>
2022-11-01 01:27:56 -0400

    Connected QuestionSets.kt to firestore database, any QuestionSets rename will automatically be updated to the database for the current user account

--- app/src/main/java/com/example/drinkinggame/QuestionSets.kt
+++ app/src/main/java/com/example/drinkinggame/QuestionSets.kt
@@ -4,12 +4,20 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat.setBackgroundTintList
 import com.example.drinkinggame.databinding.ActivityQuestionSetsBinding
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 
 var questionsetselected = 0
@@ -18,7 +26,11 @@ var questionsetrename = 0
 
 class QuestionSets : AppCompatActivity() {
     private lateinit var binding: ActivityQuestionSetsBinding
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var qsn1: String
+    private lateinit var qsn2: String
+    private lateinit var qsn3: String
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionSetsBinding.inflate(layoutInflater)
@@ -26,6 +38,28 @@ class QuestionSets : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+        //Setting Default/Saved Values
+        auth = FirebaseAuth.getInstance()
+        val qSetNamesref = db.collection("Account Data").document(auth.currentUser?.uid.toString()).collection(
+                "Question Set Name Edit"
+        ).document("Names").addSnapshotListener{ snapshot, e ->
+            if (e != null) {
+                Log.w("Main", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                Log.d("Main", "Current data: ${snapshot.data}")
+                binding.Qs1.text = snapshot.getString("QS1Name").toString()
+                binding.Qs2.text = snapshot.getString("QS2Name").toString()
+                binding.Qs3.text = snapshot.getString("QS3Name").toString()
+            } else {
+                Log.d("Main", "Current data: null")
+            }
+        }
+        ///End of Setting Default/Saved Question Set Names
+
+        //Buttons for Edit,Rename,Select
         binding.e1.setOnClickListener {
             questionsetedit = 1
             val intent = Intent(this, EditQuestionSet::class.java)
@@ -72,7 +106,18 @@ class QuestionSets : AppCompatActivity() {
             binding.s3.text = "Selected"
             binding.s3.backgroundTintList= ContextCompat.getColorStateList(applicationContext, R.color.DarkViolet)
         }
+        //End of Buttons for Edit,Rename,Select
     }
+    private fun updateUI(){
+        var qSetNamesref = db.collection("Account Data").document(auth.currentUser?.uid.toString()).collection(
+                "Question Set Name Edit"
+        ).document("Names")
+        qSetNamesref.update("QS1Name", binding.Qs1.text.toString())
+        qSetNamesref.update("QS2Name", binding.Qs2.text.toString())
+        qSetNamesref.update("QS3Name", binding.Qs3.text.toString())
+        Log.d("Main", "updateUI worked")
+    }
+
     private fun clearSelect(){
         binding.s1.text = buildString {
         append("Select")
@@ -100,10 +145,12 @@ class QuestionSets : AppCompatActivity() {
                     // positive button text and action
                     .setPositiveButton("Confirm", DialogInterface.OnClickListener { dialog, id ->
                         binding.Qs1.text = input.text.toString()
+                        updateUI()
                     })
                     // negative button text and action
                     .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
                         dialog.cancel()
+
                     })
             }
             2 -> {
@@ -113,10 +160,12 @@ class QuestionSets : AppCompatActivity() {
                     // positive button text and action
                     .setPositiveButton("Confirm", DialogInterface.OnClickListener { dialog, id ->
                         binding.Qs2.text = input.text.toString()
+                        updateUI()
                     })
                     // negative button text and action
                     .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
                         dialog.cancel()
+
                     })
             }
             3 -> {
@@ -126,10 +175,12 @@ class QuestionSets : AppCompatActivity() {
                     // positive button text and action
                     .setPositiveButton("Confirm", DialogInterface.OnClickListener { dialog, id ->
                         binding.Qs3.text = input.text.toString()
+                        updateUI()
                     })
                     // negative button text and action
                     .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
                         dialog.cancel()
+
                     })
             }
         }
