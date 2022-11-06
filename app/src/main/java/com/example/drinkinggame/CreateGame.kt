db7c941690ee73270161d0d359c15d86275b84ae
Windx15 <17182149+windx15@users.noreply.github.com>
2022-11-06 02:10:06 -0500

    Database connected to CreateGame.kt and will add a game created, need to work on room code verification

--- app/src/main/java/com/example/drinkinggame/CreateGame.kt
+++ app/src/main/java/com/example/drinkinggame/CreateGame.kt
@@ -9,17 +9,20 @@ import android.view.View
 import android.widget.Toast
 import com.example.drinkinggame.databinding.ActivityCreateGameBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 var timer = 0
 var maxPlayer = 0
 var roomCode = ""
 var host = ""
+
 class CreateGame : AppCompatActivity() {
     private lateinit var binding: ActivityCreateGameBinding
     private lateinit var auth: FirebaseAuth
     val db = Firebase.firestore
-    val isValid = false
+    private var isValid = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateGameBinding.inflate(layoutInflater)
@@ -38,12 +41,16 @@ class CreateGame : AppCompatActivity() {
 
             if (snapshot != null && snapshot.exists()) {
                 Log.d("Main", "Current data: ${snapshot.data}")
-                if(questionsetselected == 1){
-                    binding.setSelected.text = snapshot.getString("QS1Name").toString()
-                }else if(questionsetselected == 2){
-                    binding.setSelected.text = snapshot.getString("QS2Name").toString()
-                }else if(questionsetselected == 3){
-                    binding.setSelected.text = snapshot.getString("QS3Name").toString()
+                when (questionsetselected) {
+                    1 -> {
+                        binding.setSelected.text = snapshot.getString("QS1Name").toString()
+                    }
+                    2 -> {
+                        binding.setSelected.text = snapshot.getString("QS2Name").toString()
+                    }
+                    3 -> {
+                        binding.setSelected.text = snapshot.getString("QS3Name").toString()
+                    }
                 }
 
             } else {
@@ -51,23 +58,70 @@ class CreateGame : AppCompatActivity() {
             }
         }
         binding.create.setOnClickListener {
+            validSettings()
+            ifRoomExist()
             createGameSettings()
         }
 
     }
+
     private fun createGameSettings(){
+        auth = FirebaseAuth.getInstance()
         timer = binding.timer.text.toString().toInt()
         maxPlayer = binding.players.text.toString().toInt()
         roomCode = binding.roomcode.text.toString()
         host = auth.currentUser?.uid.toString()
-
+        val data = hashMapOf(
+            "Host" to host,
+            "Player 1" to host,
+            "PLayer 2" to "",
+            "PLayer 3" to "",
+            "PLayer 4" to "",
+            "PLayer 5" to "",
+            "PLayer 6" to "",
+        )
+        db.collection("Games Created").document(roomCode).set(data)
         val intent = Intent(this, ActiveGame::class.java)
         startActivity(intent)
 
     }
-    private fun validate(){
+    private fun validSettings(){
+        timer = binding.timer.text.toString().toInt()
+        maxPlayer = binding.players.text.toString().toInt()
+
+            if( timer < 10){
+                binding.cErrorInfo2.visibility = View.VISIBLE
+            }else if (maxPlayer > 6 || maxPlayer < 2 ){
+                binding.cErrorInfo.visibility = View.VISIBLE
+            }else if(binding.roomcode.text.isEmpty()){
+                Toast.makeText(applicationContext, "Please enter a room code", Toast.LENGTH_SHORT).show()
+            }
+            binding.cErrorInfo.visibility = View.INVISIBLE
+            binding.cErrorInfo2.visibility = View.INVISIBLE
+
 
     }
+    private fun ifRoomExist() {
+        roomCode = binding.roomcode.text.toString()
+
+            val gameCode = db.collection("Games Created").document(roomCode)
+            gameCode.get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    if (document.exists()) {
+                        Log.d("TAG", "Document already exists.")
+                        Toast.makeText(applicationContext, "Room Code Taken", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        Log.d("TAG", "Document doesn't exist.")
+
+                    }
+                } else {
+                    Log.d("TAG", "Error: ", task.exception)
+                }
+            }
 
 
+
+    }
 }
