7158629e528484073f941208ed37d9386e359a7e
Windx15 <17182149+windx15@users.noreply.github.com>
2022-11-08 21:17:16 -0500

    merged with animated safe screen and did some minor tweaks to fit to scale

--- app/src/main/java/com/example/drinkinggame/SafeScreen.kt
+++ app/src/main/java/com/example/drinkinggame/SafeScreen.kt
@@ -7,5 +7,8 @@ class SafeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_safe_screen)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
     }
 }
