e01881c0b48b1fbb59987ae7106960678388eee5
Windx15 <17182149+windx15@users.noreply.github.com>
2022-11-09 23:55:51 -0500

    did change display name popup and now need to connect to database

--- app/src/main/java/com/example/drinkinggame/InitialScreen.kt
+++ app/src/main/java/com/example/drinkinggame/InitialScreen.kt
@@ -1,22 +1,33 @@
 package com.example.drinkinggame
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
+import org.w3c.dom.Text
+var selectedIcon = ""
 class InitialScreen : AppCompatActivity() {
     private lateinit var jGame: Button
     private lateinit var cQuestions: Button
     private lateinit var auth: FirebaseAuth
     private lateinit var bLogout: Button
     private lateinit var userUID: TextView
-    private lateinit var cGame: Button
+    private lateinit var cGame: AppCompatButton
+    private lateinit var cName: AppCompatButton
+    private lateinit var cIcon: AppCompatButton
+    private lateinit var displayName: TextView
+    private lateinit var userIcon: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initialscreen)
@@ -29,6 +40,16 @@ class InitialScreen : AppCompatActivity() {
         bLogout = findViewById(R.id.log_out)
         userUID = findViewById(R.id.userUID)
         cGame = findViewById(R.id.createGame)
+        cName = findViewById(R.id.changeNameB)
+        cIcon = findViewById(R.id.changeIconB)
+        displayName = findViewById(R.id.displayname)
+        userIcon = findViewById(R.id.iconimginitial)
+
+        cIcon.setOnClickListener {
+            val intent = Intent(this,AvatarIcons::class.java)
+            startActivity(intent)
+        }
+        cName.setOnClickListener { changeName() }
         jGame.setOnClickListener {
             val intent = Intent(this, ActiveGame::class.java)
             startActivity(intent)
@@ -45,6 +66,22 @@ class InitialScreen : AppCompatActivity() {
         }
         bLogout.setOnClickListener { logout() }
 
+    }
+    private fun changeName(){
+        val input = EditText(this)
+        input.hint = "Type Here"
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage("Change Your Display Name")
+            .setView(input)
+            .setCancelable(true)
+            .setPositiveButton("Confirm",DialogInterface.OnClickListener{dialog, id ->
+                displayName.text = input.text.toString()
+            }).setNegativeButton("Cancel", DialogInterface.OnClickListener{dialog, id ->
+                dialog.cancel()
+            })
+        val alert = dialogBuilder.create()
+        // show alert dialog
+        alert.show()
     }
 
     override fun onStart() {
