27783456b98536d550c3d284f33ac479ee5b137d
MacNCheze <krauthr87@gmail.com>
2022-11-17 18:18:42 -0500

    gyro and timer work need to test/fix camera view

--- app/src/main/java/com/example/drinkinggame/CameraMiniGame.kt
+++ app/src/main/java/com/example/drinkinggame/CameraMiniGame.kt
@@ -1,14 +1,163 @@
 package com.example.drinkinggame
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.hardware.*
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.provider.MediaStore
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+
 
 class CameraMiniGame : AppCompatActivity() {
+    lateinit var textX: TextView
+    lateinit var textZ:TextView
+    lateinit var sensorManager: SensorManager
+    lateinit var timertext: TextView
+    lateinit var sensor: Sensor
+    private var mCamera: Camera? = null
+    private var mPreview: CameraPreview? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera_mini_game)
         if (supportActionBar != null) {
             supportActionBar!!.hide()
+        }
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        textX = findViewById(R.id.textX)
+        textZ = findViewById(R.id.textZ)
+        timertext=findViewById(R.id.timer)
+        fun getCameraInstance(): Camera? {
+            return try {
+                Camera.open() // attempt to get a Camera instance
+            } catch (e: Exception) {
+                // Camera is not available (in use or does not exist)
+                null // returns null if camera is unavailable
+            }
+        }
+        // Create an instance of Camera
+        mCamera = getCameraInstance()
+
+        mPreview = mCamera?.let {
+            // Create our Preview view
+            CameraPreview(this, it)
+        }
+
+        // Set the Preview view as the content of our activity.
+        mPreview?.also {
+            val preview: FrameLayout = findViewById(R.id.camera_preview)
+            preview.addView(it)
+        }
+
+        object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timertext.text = "seconds remaining: " + millisUntilFinished / 1000
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@CameraMiniGame, SafeScreen::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.start()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(gyroListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorManager!!.unregisterListener(gyroListener)
+    }
+
+    var gyroListener: SensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, acc: Int) {}
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val z = event.values[2]
+            textX!!.setText("X : " + x.toInt() + " rad/s")
+            textZ!!.setText("Z : " + z.toInt() + " rad/s")
+            if(x>10||z>10||x<-10||z<-10)
+            {
+                val intent = Intent(this@CameraMiniGame, FailScreen::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
+
+/** A basic Camera preview class */
+class CameraPreview(
+    context: Context,
+    private val mCamera: Camera
+) : SurfaceView(context), SurfaceHolder.Callback {
+
+    private val mHolder: SurfaceHolder = holder.apply {
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        addCallback(this@CameraPreview)
+        // deprecated setting, but required on Android versions prior to 3.0
+        setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        mCamera.apply {
+            try {
+                setPreviewDisplay(holder)
+                startPreview()
+            } catch (e: IOException) {
+                Log.d(TAG, "Error setting camera preview: ${e.message}")
+            }
+        }
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        if (mHolder.surface == null) {
+            // preview surface does not exist
+            return
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview()
+        } catch (e: Exception) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        mCamera.apply {
+            try {
+                setPreviewDisplay(mHolder)
+                startPreview()
+            } catch (e: Exception) {
+                Log.d(TAG, "Error starting camera preview: ${e.message}")
+            }
         }
     }
 }
