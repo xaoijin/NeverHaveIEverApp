3a4ec7b20ad779c98657a38e9f9ba47c38ea5301
MacNCheze <krauthr87@gmail.com>
2022-11-23 19:58:31 -0500

    camera still doesnt work for some reason

--- app/src/main/java/com/example/drinkinggame/CameraMiniGame.kt
+++ app/src/main/java/com/example/drinkinggame/CameraMiniGame.kt
@@ -1,28 +1,23 @@
 package com.example.drinkinggame
 
-
-import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
 import android.hardware.*
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.provider.MediaStore
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.FrameLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.IOException
 
 
 class CameraMiniGame : AppCompatActivity() {
     lateinit var textX: TextView
-    lateinit var textZ:TextView
+    lateinit var textY:TextView
     lateinit var sensorManager: SensorManager
     lateinit var timertext: TextView
     lateinit var sensor: Sensor
@@ -38,7 +33,7 @@ class CameraMiniGame : AppCompatActivity() {
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
         textX = findViewById(R.id.textX)
-        textZ = findViewById(R.id.textZ)
+        textY = findViewById(R.id.textY)
         timertext=findViewById(R.id.timer)
         fun getCameraInstance(): Camera? {
             return try {
@@ -89,10 +84,10 @@ class CameraMiniGame : AppCompatActivity() {
         override fun onAccuracyChanged(sensor: Sensor?, acc: Int) {}
         override fun onSensorChanged(event: SensorEvent) {
             val x = event.values[0]
-            val z = event.values[2]
+            val y = event.values[1]
             textX!!.setText("X : " + x.toInt() + " rad/s")
-            textZ!!.setText("Z : " + z.toInt() + " rad/s")
-            if(x>10||z>10||x<-10||z<-10)
+            textY!!.setText("Y : " + y.toInt() + " rad/s")
+            if(x>10||y>10||x<-10||y<-10)
             {
                 val intent = Intent(this@CameraMiniGame, FailScreen::class.java)
                 startActivity(intent)
